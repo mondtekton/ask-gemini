7a7feb9fa20936cdc73d41b3ba1466f295938e31
MondTekton <mondodev88@gmail.com>
2025-04-08 12:58:27 +0000

    Updated Gemini API Key

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,6 +4,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
+
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
@@ -24,7 +25,6 @@ async function runChat(prompt) {
   });
 
   const result = await chatSession.sendMessage(prompt);
-  console.log(result.response.text());
   return result.response.text();
 }
 
